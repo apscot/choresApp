15ecf64c86fd8611698dd9a43484a7a0cac5235f
Apscot <dontno24@gmail.com>
2020-01-04 17:37:27 +0530

    initiation

---
+++ src/main/java/choresApp/choresHelloWorldTest.java
@@ -0,0 +1,13 @@
+package choresApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class choresHelloWorldTest {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(choresHelloWorldTest.class, args);
+	}
+
+}
---
+++ src/main/java/choresApp/controller/TestController.java
@@ -0,0 +1,13 @@
+package choresApp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping(value="/")
+	public String method1() {
+		return "Hello World!";
+	}
+}
