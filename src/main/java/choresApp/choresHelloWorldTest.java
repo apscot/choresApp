cc007a3244c8570883d431346ca8d9fd7866705a
Apscot <dontno24@gmail.com>
2020-01-04 17:43:58 +0530

    small bug fixes

--- src/main/java/choresApp/choresHelloWorldTest.java
+++ src/main/java/choresApp/choresHelloWorldTest.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class choresHelloWorldTest {
+public class ChoresHelloWorldTest {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(choresHelloWorldTest.class, args);
+		SpringApplication.run(ChoresHelloWorldTest.class, args);
 	}
 
 }
